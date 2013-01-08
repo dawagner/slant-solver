c88870fc640619d4c713d3c36e275c74df8e081b
David Wagner <deubeuliou@giskard.(none)>
2013-01-09 00:58:26 +0100

    Cleaner. introduce 2-dims tables
    Signed-off-by: David Wagner <deubeuliou@giskard.(none)>

--- slant.py
+++ slant.py
@@ -3,7 +3,7 @@ import sys
 
 class Board():
 	def __init__(self, size):
-		self.board = set()
+		self.board = [[None for i in range(size)] for i in range(size)]
 		self.size = size
 
 		Road.init(size)
@@ -11,16 +11,20 @@ class Board():
 		# we need to create the first node (at the bottom-right) as if
 		# we come from outside the board toward the second quadrant.
 		# See Corner.__init__
+		# The first node can walk to half of the other nodes;
+		# We need a second node to create the other half.
+		# To convince yourself that there cannot be less than two
+		# separate roads, start from any node and by making only
+		# diagonal moves, try to cover the whole board.  You won't be
+		# able to.
 		Corner.create(self, size-1, size-1, 2)
+		Corner.create(self, size-2, size-1, 2)
 
 	def add(self, corner):
-		self.board.add(corner)
+		self.board[corner.x][corner.y] = corner
 
 	def get(self, x, y):
-		clone = Corner(x, y)
-		for c in self.board:
-			if c == clone:
-				return c
+		return self.board[x][y]
 
 	def __repr__(self):
 		return self.board.__repr__()
@@ -52,22 +56,28 @@ class Corner():
 	def __repr__(self):
 		return "<Corner (%d, %d): %s>" % (self.x, self.y, self.hintValue)
 
-	def _get_quadrant_coords(self, quadrant):
-		addVector = [(1, -1), (-1, -1), (-1, 1), (1, 1)][quadrant-1]
-		return self.x + addVector[0], self.y + addVector[1]
+	def get_neighbor_coords(self, quadrant):
+		addVector = [(1, -1), (-1, -1), (-1, 1), (1, 1)]
+		add = addVector[quadrant-1]
+		return self.x + add[0], self.y + add[1]
+
+	def get_road_coords(self, quadrant):
+		addVector = [(0, -1), (-1, -1), (-1, 0), (0, 0)]
+		add = addVector[quadrant-1]
+		return self.x + add[0], self.y + add[1]
 
 	@staticmethod
 	def _opposite_quadrant(quadrant):
 		return (quadrant-1 + 2) % 4 + 1
 
 	def get_road(self, quadrant):
-		return self.quadrant[quadrant - 1]
+		return self.quadrants[quadrant - 1]
 
 	def get_neighbor(self, quadrant):
 		return self.get_road(quadrant).get_neighbor(self)
 
 	def do_link(self, quadrant, road):
-		""" Make %quadrand point to %road
+		""" Make %quadrant point to %road
 		"""
 		self.quadrants[quadrant - 1] = road
 
@@ -78,19 +88,18 @@ class Corner():
 		they can be.
 		Does the same operation on the neighbor.
 		"""
-		road = Road.get(*self._get_quadrant_coords(quadrant))
+		#print("node: %s->%d" % ((self.x, self.y),quadrant))
+		road = Road.get(*self.get_road_coords(quadrant))
 		road.construct(self, other)
 
 		self.do_link(quadrant, road)
 		other.do_link(Corner._opposite_quadrant(quadrant), road)
 
 	def route(self, quadrant):
-		self.get_road(quadrant).route()
-		#Road.get(_get_quadrant_coords(quadrant)).route(self, other)
+		self.get_road(quadrant).route(self)
 
 	def unroute(self, quadrant):
 		self.get_road(quadrant).unroute()
-		#Road.get(_get_quadrant_coords(quadrant)).unroute(self, other)
 
 	@classmethod
 	def create(cls, board, x, y, fromQuadrant):
@@ -104,26 +113,26 @@ class Corner():
 		fromQuadrant = c._opposite_quadrant(fromQuadrant)
 
 		def _walk(nextQuadrant):
-			coords = c._get_quadrant_coords(nextQuadrant)
+			coords = c.get_neighbor_coords(nextQuadrant)
 			if nextQuadrant != fromQuadrant:
 				c.link(cls.create(board, coords[0], coords[1], nextQuadrant), nextQuadrant)
 
 		if y > 0:
 			if x < board.size - 1:
-				print("toward 1")
+				#print("toward 1")
 				_walk(1)
 
 			if x > 0:
-				print("toward 2")
+				#print("toward 2")
 				_walk(2)
 
 		if y < board.size - 1:
 			if x > 0:
-				print("toward 3")
+				#print("toward 3")
 				_walk(3)
 			# Never walk the 4th quadrant
 
-		print("Fin du chemin pour %s" % c)
+		#print("Fin du chemin pour %s" % c)
 
 		return c
 
@@ -154,22 +163,35 @@ class Road():
 		self.connection = couple
 		other = next(c for c in couple if corner != c)
 
+	# TODO: route_other
+
 	def unroute(self):
 		""" Remove the current connection
 		"""
 		self.connection = None
 
+	def __repr__(self):
+		couples = ["***%s***" % (c,)
+				if c == self.connection
+				else "%s" % (c,)
+				for c in self.couples]
+
+		return "<Road %s>" % "; ".join(couples)
+
 	@classmethod
 	def init(cls, size):
-		cls.__roads = [[None] * size] * size
+		cls.__roads = [[None for i in range(size)] for i in range(size)]
 
 	@classmethod
 	def get(cls, x, y):
-		print(x, y)
+		#print("road: %s" % ((x, y),))
 		if not cls.__roads[x][y]:
 			cls.__roads[x][y] = Road()
 		return cls.__roads[x][y]
 
+def build(size):
+	return Board(size)
+
 if __name__ == "__main__":
-	board = Board(int(sys.argv[1]))
+	board = build(int(sys.argv[1]))
 	print(board)
