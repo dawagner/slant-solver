9c79280e805abde9d7cd8a5a52691f433f7c89af
David Wagner <david@marvid.fr>
2013-08-02 11:05:51 +0200

    OHM - 2 aout
    Signed-off-by: David Wagner <david@marvid.fr>

---
+++ ohm.hs
@@ -0,0 +1,136 @@
+-- SlantNode: / or \
+data SlantNode = Slash | BackSlash | NoSlant
+        deriving (Show)
+
+type SlantNode' = Maybe SlantNode
+
+type Hint = Maybe Int
+
+-- TopHintNode down right? hint? south-east-slant?
+-- HintNode down? hint? south-east-slant?
+data HintTree = TopHintNode {
+			down :: HintTree,
+			right :: HintTree,
+			hint:: Hint,
+			slantNode :: SlantNode'}
+              | HintNode {
+			down :: HintTree,
+			hint :: Hint,
+			slantNode :: SlantNode'}
+              | NoNode
+              deriving (Show)
+
+type TreeAddition = HintTree -> HintTree -> HintTree
+
+data SlantCrumb = DownCrumb {
+			crumbHint :: Hint,
+			crumbSlantNode :: SlantNode',
+			crumbRight :: HintTree}
+                | RightCrumb {
+			crumbHint :: Hint,
+			crumbSlantNode :: SlantNode',
+			crumbDown :: HintTree}
+                deriving (Show)
+		-- | TwoDownCrumb {
+		-- 	crumbHint :: Hint,
+		-- 	crumbSlantNode :: SlantNode'}
+
+type SlantZipper = (HintTree, [SlantCrumb])
+
+mkTopNode :: HintTree
+mkTopNode = TopHintNode NoNode NoNode Nothing Nothing 
+
+mkNode :: HintTree
+mkNode = HintNode NoNode Nothing Nothing
+
+addRight :: TreeAddition
+addRight root@TopHintNode{right = NoNode} new =
+	root{right = new}
+
+addRight TopHintNode{} _ = error "This node already has a right neighbour"
+
+addRight HintNode{} _ = error "Can't add right to a non-top Node"
+
+addDown :: TreeAddition
+addDown root@TopHintNode{down = NoNode} new = 
+	root{down = new}
+
+addDown root@HintNode{down = NoNode} new = 
+	root{down = new}
+
+addDown _ _ = error "This node already has a down neighbour"
+
+--
+-- go RIGHT --
+--
+goRight :: SlantZipper -> SlantZipper
+goRight (TopHintNode down right@TopHintNode{} hint slantNode, crumbs) =
+        (right, RightCrumb hint slantNode down : crumbs)
+
+-- TODO
+goRight (TopHintNode down NoNode hint slantNode, crumbs) = error "nothing on the right"
+
+-- TODO
+goRight node@(HintNode down hint slantNode, crumbs) =
+	goDown . goRight . goUp $ node
+
+
+--
+-- go DOWN --
+--
+goDown :: SlantZipper -> SlantZipper
+goDown (TopHintNode down right hint slantNode, crumbs) =
+	(down, DownCrumb hint slantNode right : crumbs)
+
+goDown (HintNode NoNode hint slantNode, crumbs) = error "nothing down"
+
+goDown (HintNode down hint slantNode, crumbs) =
+	(down, DownCrumb hint slantNode NoNode : crumbs)
+
+
+--
+-- go UP --
+--
+goUp :: SlantZipper -> SlantZipper
+goUp (current@HintNode{}, DownCrumb hint slantNode right@TopHintNode{} : cs) =
+	(TopHintNode{right = right, down = current, hint = hint, slantNode = slantNode},
+	 cs)
+
+goUp (current@HintNode{}, DownCrumb hint slantNode NoNode : rcs@RightCrumb{} : cs) =
+	(TopHintNode{right = NoNode, down = current, hint = hint, slantNode = slantNode},
+	 rcs : cs)
+
+goUp (current@HintNode{}, DownCrumb hint slantNode NoNode : cs) =
+	(HintNode{down = current, hint = hint, slantNode = slantNode},
+	 cs)
+
+goUp (current@TopHintNode{}, RightCrumb hint slantNode down@(HintNode{}) : cs) =
+	(TopHintNode{right = current, down = down, hint = hint, slantNode = slantNode},
+	 cs)
+
+
+goTop :: SlantZipper -> SlantZipper
+goTop zip@(node, []) = zip
+goTop zip@(node, [crumb]) = goUp zip
+goTop zip@(node, crumbs) = goTop (goUp zip)
+
+
+--
+-- change Hint At
+--
+setHintAt :: SlantZipper -> Hint -> SlantZipper
+setHintAt (node, crumbs) hint = (node{hint = hint}, crumbs)
+
+test_board :: HintTree
+test_board =
+	TopHintNode 
+		(HintNode NoNode Nothing Nothing)
+		(TopHintNode
+			(HintNode NoNode (Just 1) Nothing)
+			NoNode
+			Nothing
+			Nothing)
+		(Just 1)
+		(Just NoSlant)
+
+test_zip = (test_board, [])
